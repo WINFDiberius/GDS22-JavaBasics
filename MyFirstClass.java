3689110f7adeb0c35a4894a8ad40dae79a605915
dxsattler <daniel.sattler2@igt.com>
2022-09-23 17:05:51 +0200

    My First Class created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        // sout + tab
+        System.out.println(Arrays.toString(args));
+
+
+    }
+}
