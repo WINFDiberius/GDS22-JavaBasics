825f426cfaf33c8aa1ae9b03ed67c32e5f2b1809
dxsattler <daniel.sattler2@igt.com>
2022-09-24 14:52:27 +0200

    Second lesson Added Function to Calculator.java and ArithmeticOperations.java Also Added MyMethodsDemo.java

--- ArithmeticOperations.java
+++ ArithmeticOperations.java
@@ -6,8 +6,8 @@ public class ArithmeticOperations {
      */
     public static void main(String[] args) {
 
-        int a = 4;
-        int b = 9;
+        int a = 3;
+        int b = 2;
 
         int sum = a + b;
 
@@ -33,6 +33,18 @@ public class ArithmeticOperations {
 
         System.out.println("Zahl1 " + a + " % Zahl2 " + b + " = Ergebnis " + sum);
 
+        // a++
+        System.out.println();
+        System.out.println("a++"); // a = a + 1
+        System.out.println(a++); // ich erwarte mir 3 weil es noch nicht inkrementiert wurde
+        System.out.println("a = " + a); // Ich erwarte mir 4 weil es nach der Ausgabe inkrementiert wurde
+        System.out.println();
+
+        // b++
+        System.out.println("++b"); // b = b + 1
+        System.out.println(++b); // ich erwarte mir 3
+        System.out.println("b = " + b); // ich bin immer noch bei 3 weil es vorher inkrementiert wurde
+        System.out.println();
 
     }
 }
--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,8 @@ public class Calculator {
         String Calculator = "Calcyplus";
 
         int number1 = 9;
-        int number2 = 5;
+        int number2 = 4;
+        int zero = 0;
 
         int solution;
 
@@ -15,6 +16,49 @@ public class Calculator {
 
         System.out.println("Mein Name ist: " + Calculator + " Ergebnis: " + solution);
 
+        System.out.println("Mein Name ist: " + Calculator + " Ergebnis der Addition: " + addition(number1,number2));
+        System.out.println("Mein Name ist: " + Calculator + " Ergebnis der Subtraktion: " + subtraktion(number1,number2));
+        System.out.println("Mein Name ist: " + Calculator + " Ergebnis der Multiplikation: " + multiplikation(number1,number2));
+        System.out.println("Mein Name ist: " + Calculator + " Ergebnis der Division: " + division(number1,number2));
+        System.out.println("Mein Name ist: " + Calculator + " Ergebnis des Modulo: " + modulo(number1,number2));
+
+        System.out.println("Mein Name ist: " + Calculator + " Ergebnis der Quadrierung: " + power(number1));
     }
 
+    public static int addition(int number1, int number2){
+        int result = number1 + number2;
+        return result;
+    }
+
+    public static int subtraktion(int number1, int number2){
+        int result = number1 - number2;
+        return result;
+    }
+
+    public static int multiplikation(int number1, int number2){
+        int result = number1 * number2;
+        return result;
+    }
+
+    public static double division(int number1, double number2){
+        if(number2 != 0) {
+            double result = number1 / number2;
+            return result;
+        }
+        else{
+            System.out.println("Durch 0 kann nicht dividiert werden");
+            return 0;
+        }
+
+    }
+
+    public static int modulo(int number1, int number2){
+        int result = number1 % number2;
+        return result;
+    }
+
+    public static int power(int number1){
+        int result = number1 * number1;
+        return result;
+    }
 }
